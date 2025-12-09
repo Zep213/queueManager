6a65f48ab80a6692fdabd9f15e5a931fadafe349
Zep213 <jososephbem100@gmail.com>
2025-12-09 09:14:39 -0300

    first-Commit

---
+++ src/main/java/com/umari/queueManeger/QueueManegerApplication.java
@@ -0,0 +1,13 @@
+package com.umari.queueManeger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueManegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueManegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umari/queueManeger/QueueManegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umari.queueManeger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueManegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
